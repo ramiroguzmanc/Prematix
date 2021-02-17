f7197395c2cab804055705f3db36f8aae002bfdd
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2021-02-16 23:57:47 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return <NavigationContainer></NavigationContainer>;
+};
+
+export default App;
